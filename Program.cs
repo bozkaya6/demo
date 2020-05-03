633b7f583cd25e72d4f670006cee921e5f5d8a6d
bozkaya6 <62771156+bozkaya6@users.noreply.github.com>
2020-05-04 00:25:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demo2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+
+            x = x + y;
+            y = x - y;
+            x = x - y;
+
+            Console.WriteLine("After swapping: x = "
+                         + x + ", y = " + y);
+
+        }
+    }
+}
